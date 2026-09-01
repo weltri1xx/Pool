397271d52afa419eb79c48740ec805a588fbba28
Weltr1xx <Weltr1xx@gmail.com>
2026-09-01 12:22:05 +0500

    follow

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from backend.auth.controller import router as auth_router
 from backend.post.controller import router as posts_router
-# from backend.websocket.controller import router as websocket_router
-# from backend.chat.controller import router as chat_router
-# from backend.stories.controller import router as stories_router
-# from backend.follows.controller import router as follows_router
+from backend.vdchat.controller import router as websocket_router
+from backend.chat.controller import router as chat_router
+
 from backend.database import Base, engine
 
 app = FastAPI()
@@ -14,10 +13,9 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.include_router(auth_router)
 app.include_router(posts_router)
-# app.include_router(websocket_router)
-# app.include_router(chat_router)
-# app.include_router(stories_router)
-# app.include_router(follows_router)
+app.include_router(websocket_router)
+app.include_router(chat_router)
+
 
 @app.on_event("startup")
 async def startup():
